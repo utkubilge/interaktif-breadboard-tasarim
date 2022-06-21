b6a65d54aa58957cc55a6f859842e6badb09e0ef
utkubilge <bahautkubilge@gmail.com>
2022-06-21 10:10:33 +0300

    updated all ics

--- comps/Board.js
+++ comps/Board.js
@@ -16,31 +16,38 @@ let LOGIC = []
 //first row
 for (let i = 0; i < 30; i++) {
     for (let j = 0; j < 5; j++)
-        DIVITS.push({ x: 34 + i * 32, y: 126 + j * 32 })
+        DIVITS.push({ x: 34 + i * 32, y: 126 + j * 32 , cnt: null, val: null})
 }
 //second row
 for (let i = 0; i < 30; i++) {
     for (let j = 0; j < 5; j++)
-        DIVITS.push({ x: 34 + i * 32, y: 341 + j * 32 })
+        DIVITS.push({ x: 34 + i * 32, y: 341 + j * 32 , cnt: null, val: null})
 }
 //first io
 for (let j1 = 0; j1 < 2; j1++) {
-    for(let i1 =0; i1 < 5; i1++)
-    for (let i = 0; i < 5; i++)
-        DIVITS.push({ x: 64 + i*32 + i1*186, y: 22 + j1*32})
+    for (let i1 = 0; i1 < 5; i1++)
+        for (let i = 0; i < 5; i++) {
+            if(j1 === 0)
+            DIVITS.push({ x: 64 + i * 32 + i1 * 186, y: 22 + j1 * 32 , cnt: null, val: false})
+            else
+            DIVITS.push({ x: 64 + i * 32 + i1 * 186, y: 22 + j1 * 32 , cnt: null, val: true})
+        }
 }
 //second io
 for (let j1 = 0; j1 < 2; j1++) {
-    for(let i1 =0; i1 < 5; i1++)
-    for (let i = 0; i < 5; i++)
-        DIVITS.push({ x: 64 + i*32 + i1*186, y: 537 + j1*32})
+    for (let i1 = 0; i1 < 5; i1++)
+        for (let i = 0; i < 5; i++)
+        if(j1 === 0)
+            DIVITS.push({ x: 64 + i * 32 + i1 * 186, y: 537 + j1 * 32 , cnt: null, val: false})
+            else
+            DIVITS.push({ x: 64 + i * 32 + i1 * 186, y: 537 + j1 * 32 , cnt: null, val: true})
 }
 
 
 const ic1c = { x: 1080, y: 30, width: 212, height: 97 };
-const ic2c = { x: 1080, y: 30 + 97 + 20, length: 212, height: 97 };
-const ic3c = { x: 1080, y: 30 + 97 * 2 + 20 * 2, length: 212, height: 97 };
-const ic4c = { x: 1080, y: 30 + 97 * 3 + 20, length: 212, height: 97 };
+const ic2c = { x: 1080, y: 30 + 97 + 20, width: 212, height: 97 };
+const ic3c = { x: 1080, y: 30 + 97 * 2 + 20 * 2, width: 212, height: 97 };
+//const ic4c = { x: 1080, y: 30 + 97 * 3 + 20, width: 212, height: 97 };
 
 const led1c = { x: 1372, y: 50, width: 32, height: 61 };
 const switch1c = { x: 1345, y: 170, width: 100, height: 45 };
@@ -67,7 +74,7 @@ export default function Board() {
 
         //very scary loop oooh
         function render() {
-            
+
             //board prerender
             ctx.fillStyle = "#c6c6c8";
             ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -143,34 +150,35 @@ function onMouseDown(evt) {
 
     //handle copying pieces
     handleCopying(evt);
-
+    SELECTED_PIECE = getPressedPiece(evt);
 
 
     //divit select
     SELECTED_DIVIT = getPressedDivit(evt);
-    if(SELECTED_DIVIT != null ) {
-    console.log("selected divit: " + SELECTED_DIVIT.x + " " + SELECTED_DIVIT.y)
-    }
-
-    //wire trashing
-    let fullwire = null;
     if (SELECTED_DIVIT != null) {
-        fullwire = getPressedWire(SELECTED_DIVIT)
+        console.log("selected divit: " + SELECTED_DIVIT.x + " " + SELECTED_DIVIT.y)
     }
-    if (fullwire != null) {
-        WIRES.splice(WIRES.indexOf(fullwire), 1)
-    }
+    
+    //wire trashing
+    
+        let fullwire = null;
+        if (SELECTED_DIVIT != null) {
+            fullwire = getPressedWire(SELECTED_DIVIT)
+        }
+        if (fullwire != null) {
+            WIRES.splice(WIRES.indexOf(fullwire), 1)
+        }
 
-    //wire drawing
-    if (SELECTED_DIVIT != null && SELECTED_WIRE == null) {
-        OLD_DIVIT = SELECTED_DIVIT;
-        SELECTED_WIRE = new Wire(evt.clientX, evt.clientY, evt.clientX, evt.clientY)
-        WIRES.push(SELECTED_WIRE);
-    }
+        //wire drawing
+        if (SELECTED_DIVIT != null && SELECTED_WIRE == null) {
+            OLD_DIVIT = SELECTED_DIVIT;
+            SELECTED_WIRE = new Wire(evt.clientX, evt.clientY, evt.clientX, evt.clientY)
+            WIRES.push(SELECTED_WIRE);
+        }
 
+    
 
-
-    SELECTED_PIECE = getPressedPiece(evt);
+    
     if (SELECTED_PIECE != null) {
         const index = PIECES.indexOf(SELECTED_PIECE);
         SELECTED_PIECE.offset = {
@@ -198,9 +206,6 @@ function onMouseUp(evt) {
     } else if (SELECTED_WIRE != null) {
         WIRES.pop()
         SELECTED_WIRE = null;
-
-
-
     }
     //piece trashing
     if (evt.clientX > 1020 && evt.clientX < 1500 && SELECTED_PIECE != null) {
@@ -217,19 +222,32 @@ function handleCopying(evt) {
     //ic1
     if (evt.clientX > ic1c.x && evt.clientX < ic1c.x + ic1c.width &&
         evt.clientY > ic1c.y && evt.clientY < ic1c.y + ic1c.height && SELECTED_PIECE == null) {
-        
+
         SELECTED_PIECE = new Piece(evt.clientX - ic1c.width / 2, evt.clientY - ic1c.height / 2, ic1c.width, ic1c.height, 'ic.svg')
         PIECES.push(SELECTED_PIECE);
     }
+    if (evt.clientX > ic2c.x && evt.clientX < ic2c.x + ic2c.width &&
+        evt.clientY > ic2c.y && evt.clientY < ic2c.y + ic2c.height && SELECTED_PIECE == null) {
+
+        SELECTED_PIECE = new Piece(evt.clientX - ic2c.width / 2, evt.clientY - ic2c.height / 2, ic2c.width, ic2c.height, 'ic.svg')
+        PIECES.push(SELECTED_PIECE);
+    }
+    if (evt.clientX > ic3c.x && evt.clientX < ic3c.x + ic3c.width &&
+        evt.clientY > ic3c.y && evt.clientY < ic3c.y + ic3c.height && SELECTED_PIECE == null) {
+
+        SELECTED_PIECE = new Piece(evt.clientX - ic3c.width / 2, evt.clientY - ic3c.height / 2, ic3c.width, ic3c.height, 'ic.svg')
+        PIECES.push(SELECTED_PIECE);
+    }
+    //led1
     if (evt.clientX > led1c.x && evt.clientX < led1c.x + led1c.width &&
         evt.clientY > led1c.y && evt.clientY < led1c.y + led1c.height && SELECTED_PIECE == null) {
-        
+
         SELECTED_PIECE = new Piece(evt.clientX - led1c.width / 2, evt.clientY - led1c.height / 2, led1c.width, led1c.height, 'ledoff.svg')
         PIECES.push(SELECTED_PIECE);
     }
     if (evt.clientX > switch1c.x && evt.clientX < switch1c.x + switch1c.width &&
         evt.clientY > switch1c.y && evt.clientY < switch1c.y + switch1c.height && SELECTED_PIECE == null) {
-        
+
         SELECTED_PIECE = new Piece(evt.clientX - switch1c.width / 2, evt.clientY - switch1c.height / 2, switch1c.width, switch1c.height, 'switchoff.svg')
         PIECES.push(SELECTED_PIECE);
     }
